5e88d39e0fa6e72d08dee4ca93f687496e0256ba
Lindan Thillanayagam <lindthilldev@gmail.com>
2024-06-16 22:46:30 -0400

    Created init sql file.

---
+++ sql/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS public.coefficient
+(
+    name text COLLATE pg_catalog."default" NOT NULL,
+    value numeric(10,2),
+    CONSTRAINT coefficient_pkey PRIMARY KEY (name)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.coefficient
+    OWNER to postgres;
+
+
+insert into public.coefficient(name, value)
+values ('distance', 0.5);
+
+insert into public.coefficient(name, value)
+values ('package', 0.5);
